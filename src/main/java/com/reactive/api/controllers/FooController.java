74aa42ba6aa7ecc694882b6189d8dd9aced03673
Natanael Junior <natanaelmarkesjr@gmail.com>
2023-07-09 12:47:00 -0300

    Updated code with an additional endpoint for findById in the controller

--- src/main/java/com/reactive/api/controllers/FooController.java
+++ src/main/java/com/reactive/api/controllers/FooController.java
@@ -1,10 +1,14 @@
 package com.reactive.api.controllers;
 
 import com.reactive.api.models.Foo;
+import com.reactive.api.repository.FooRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.time.Duration;
 
@@ -12,10 +16,23 @@ import java.time.Duration;
 @RequestMapping("/api")
 public class FooController {
 
+    private final FooRepository fooRepository;
+
+    @Autowired
+    public FooController(FooRepository fooRepository) {
+        this.fooRepository = fooRepository;
+    }
+
     @GetMapping("/foo")
     public Flux<Foo> getFoo() {
-        return Flux.interval(Duration.ofSeconds(1))
-                .map(i -> new Foo(i.intValue(), "Foo " + i))
+        return fooRepository.findAll()
+                .delayElements(Duration.ofSeconds(1))
                 .take(10);
     }
+
+    @GetMapping("/foo/{id}")
+    public Mono<Foo> getAPIById(@PathVariable("id") int id) {
+        return fooRepository.findById(id);
+    }
+    
 }
