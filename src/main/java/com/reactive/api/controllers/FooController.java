a9791e087f2ad2a842d771f6b3547c617d0bc0fc
Natanael Junior <natanaelmarkesjr@gmail.com>
2023-07-09 14:19:48 -0300

    Emit a new Foo event every 1 second through the /foo/events endpoint.

--- src/main/java/com/reactive/api/controllers/FooController.java
+++ src/main/java/com/reactive/api/controllers/FooController.java
@@ -3,6 +3,7 @@ package com.reactive.api.controllers;
 import com.reactive.api.models.Foo;
 import com.reactive.api.repository.FooRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,7 +14,7 @@ import reactor.core.publisher.Mono;
 import java.time.Duration;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/foo")
 public class FooController {
 
     private final FooRepository fooRepository;
@@ -23,16 +24,18 @@ public class FooController {
         this.fooRepository = fooRepository;
     }
 
-    @GetMapping("/foo")
+    @GetMapping(value = "/events", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Foo> getFooEvents() {
+        return Flux.interval(Duration.ofSeconds(1)).map(i -> new Foo(i.intValue(), "Foo " + i));
+    }
+    
+    @GetMapping(value="/")
     public Flux<Foo> getFoo() {
-        return fooRepository.findAll()
-                .delayElements(Duration.ofSeconds(1))
-                .take(10);
+        return fooRepository.findAll().take(10);
     }
 
-    @GetMapping("/foo/{id}")
+    @GetMapping("/{id}")
     public Mono<Foo> getFooById(@PathVariable("id") int id) {
         return fooRepository.findById(id);
     }
-    
 }
